0fedc96bd3c1690c2cfce2537a94518a992059db
Antonio Mata <antonio.mata.sv@gmail.com>
2024-05-03 20:23:32 +0200

    feat: add initial framework for BlockChain

--- main.go
+++ main.go
@@ -1,10 +1,57 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"fmt"
-	"rsc.io/quote"
 )
 
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+func (blockchain *BlockChain) AddBlock(data string) {
+	previousBlock := blockchain.blocks[len(blockchain.blocks)-1]
+	newBlock := CreateBlock(data, previousBlock.Hash)
+	blockchain.blocks = append(blockchain.blocks, newBlock)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{blocks: []*Block{Genesis()}}
+}
+
 func main() {
-	fmt.Println(quote.Hello())
+	blockChain := InitBlockChain()
+
+	blockChain.AddBlock("First block after Genesis")
+	blockChain.AddBlock("Second block after Genesis")
+	blockChain.AddBlock("Third block after Genesis")
+
+	for _, block := range blockChain.blocks {
+		fmt.Printf("Previous hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n\n", block.Hash)
+	}
 }
